3dc215aff52e50ec4b629f234f8aba7171a51aa2
Linnnnnn42 <Rev0lve@outlook.com>
2025-08-12 09:12:56 +0800

    Project setup

---
+++ src/constants/tokens.ts
@@ -0,0 +1,18 @@
+export const colors = {
+    primary: '#43fca0',
+    background: '#fff',
+    text: '#000',
+    textMuted: '#707070',
+    icon: '#fff',
+}
+
+export const fontSize = {
+    xs: 12,
+    sm: 16,
+    base: 20,
+    lg: 24,
+}
+
+export const screenPadding = {
+    horizontal: 24,
+}
